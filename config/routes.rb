8449106aa8d26ba604bdbc103f8912896dd040b4
ryotakida <uulee5521@gmail.com>
2021-09-14 15:06:34 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/edit'
+  get 'books/index'
      devise_for :users
      root 'homes#index'     
+     resource :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_074850) do
+ActiveRecord::Schema.define(version: 2021_09_14_112940) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
