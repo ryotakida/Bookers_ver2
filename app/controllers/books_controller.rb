e6973f4dae5a8c1cc8c287314bb67dfc637181db
ryotakida <uulee5521@gmail.com>
2021-09-14 18:12:24 +0000

    muzukashii

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,43 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
-  def creater
+  def create
+    @book = PostImage.new(post_image_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book)
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(book)
+  end  
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
+
+  private
+
+  def post_image_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end  
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/edit'
-  get 'books/index'
+
      devise_for :users
      root 'homes#index'     
-     resource :books
+     resources :books
+     
+     
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
