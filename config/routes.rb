12d5f5e9af07d2efbcb0fb170ae983f2bda11599
ryotakida <uulee5521@gmail.com>
2021-09-15 06:19:45 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-     devise_for :users
-     root 'homes#index'     
-     resources :books
+
+  devise_for :users
+  root 'homes#index'     
+  resources :books
      
      
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+ 
+ resources :users, only: [:show,:edit,:update]
+ 
+end 
