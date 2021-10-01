0771c1146c45641d411e1271a5b936e5ad0d303b
ryotakida <uulee5521@gmail.com>
2021-10-01 07:31:43 +0000

    comment after

--- app/controllers/books_comments_controller.rb
+++ app/controllers/books_comments_controller.rb
@@ -2,21 +2,23 @@ class BooksCommentsController < ApplicationController
   def create
 
     @book = Book.find(params[:book_id])
-    @books_comment = current_user.books_comments.new(books_comment_params)
-    
+    @books_comment = BooksComment.new(books_comment_params)
+    @books_comment.user_id = current_user.id
+    @books_comment.book_id = @book.id
     
     if @books_comment.save
-      redirect_to book_path(@book)   
+      redirect_to book_path(params[:book_id])   
     else
-      render "books/show"
-    end  
+     render "books/show"
+    end   
   end
 
   def destroy
-      
+    BooksComment.find_by(id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id]) 
   end
   private
   def books_comment_params
-    params.require(:books_comment).permit(:comment, :book_id, :user_id)
+    params.require(:books_comment).permit(:comment)
   end  
 end
