a6e9abb8c4fce0e184d2fa96fed4eb6c47b60079
ryotakida <uulee5521@gmail.com>
2021-10-06 18:52:09 +0000

    ??画面が換位しない

--- app/controllers/books_comments_controller.rb
+++ app/controllers/books_comments_controller.rb
@@ -2,20 +2,16 @@ class BooksCommentsController < ApplicationController
   def create
 
     @book = Book.find(params[:book_id])
-    @books_comment = BooksComment.new(books_comment_params)
+    @books_comment = @book.books_comments.build(books_comment_params)
     @books_comment.user_id = current_user.id
     @books_comment.book_id = @book.id
-    
-    if @books_comment.save
-      redirect_to book_path(params[:book_id])   
-    else
-     render "books/show"
-    end   
+    @books_comment.save
+    render "books/show"
   end
 
   def destroy
     BooksComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id]) 
+    render "books/show"
   end
   private
   def books_comment_params
